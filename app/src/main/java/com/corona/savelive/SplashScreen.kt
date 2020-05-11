8262ab873a797a6325aba3584b8d9edb1cfc080f
hendra0ma <hendra0maulidan@gmail.com>
2020-05-11 20:42:52 +0700

    Make a SplashScreen

---
+++ app/src/main/java/com/corona/savelive/SplashScreen.kt
@@ -0,0 +1,17 @@
+package com.corona.savelive
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+        },1000)
+    }
+}
