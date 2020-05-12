644a21d58534f8f72c49ff9d84e36f374e8418b6
hendra0ma <hendra0maulidan@gmail.com>
2020-05-12 22:21:14 +0700

    Selesai Membuat Fragment

---
+++ app/src/main/java/com/corona/savelive/KotakAdapter.kt
@@ -0,0 +1,21 @@
+package com.corona.savelive
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class KotakAdapter (fragmentActivity: FragmentActivity):FragmentStateAdapter(fragmentActivity) {
+    private val menu = 3
+    override fun createFragment(posisi: Int): Fragment {
+       return when(posisi){
+             0 -> CovidFragment()
+           1-> KonsultasiFragment()
+           2-> ReportVirusFragment()
+           else->CovidFragment()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return menu
+    }
+}
--- app/src/main/java/com/corona/savelive/MainActivity.kt
+++ app/src/main/java/com/corona/savelive/MainActivity.kt
@@ -2,11 +2,29 @@ package com.corona.savelive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    val icon = arrayOf(R.drawable.ic_newspaper,R.drawable.ic_chat_black_24dp,R.drawable.report_virus)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = KotakAdapter(this)
+        kotak.setAdapter(adapter)
+        TabLayoutMediator(menuLayout,kotak, TabLayoutMediator.TabConfigurationStrategy {
+                tab, position ->
+            tab.icon = ResourcesCompat.getDrawable(resources,icon[position],null)
+        }).attach()
+
+
+
     }
+
+
 }
