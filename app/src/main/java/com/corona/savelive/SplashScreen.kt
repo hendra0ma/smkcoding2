7c2ff623e4b212af0964dad870dc12806342452c
hendra0ma <hendra0maulidan@gmail.com>
2020-05-13 23:12:32 +0700

    Membuat Layout untuk Aplikasi 1

--- app/src/main/java/com/corona/savelive/SplashScreen.kt
+++ app/src/main/java/com/corona/savelive/SplashScreen.kt
@@ -12,6 +12,7 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
         Handler().postDelayed({
             startActivity(Intent(this,MainActivity::class.java))
+            finish()
         },1000)
     }
 }
